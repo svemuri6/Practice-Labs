dd2a23d4f0ec7f30521cb229d139b2c56f54eceb
sanjeevi <sanjeevi.vemuri@gmail.com>
2019-05-19 15:39:43 -0700

    Initial File Setup

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// 
+
+var likesGamesOfThrones = prompt('Do you like game of thrones?'); 
+likesGamesOfThrones = likesGamesOfThrones.toLowerCase(); 
+if (likesGamesOfThrones === "yes" ){
+  alert("Thats Awesome"); 
+}else if (likesGamesOfThrones === "somewhat"){
+  alert("I agree its Ok")
+}else{
+  alert ('That sucks'); 
+}
+
+// var likesGamesOfThrones = prompt('Do you like game of thrones?'); 
+// likesGamesOfThrones = likesGamesOfThrones.toUpperCase(); 
+// if (likesGamesOfThrones === "YES" ){
+//   alert("Thats Awesome"); 
+// }else if (likesGamesOfThrones === "somewhat"){
+//   alert("I agree its Ok"); 
+// }else{
+//   alert ('That sucks'); 
+// }
