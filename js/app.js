b5632f29b820b48fe387ad6fc3c75f3e688cdb38
sanjeevi <sanjeevi.vemuri@gmail.com>
2019-05-19 22:57:45 -0700

    test

--- js/app.js
+++ js/app.js
@@ -1,21 +1,16 @@
-// 
 
-var likesGamesOfThrones = prompt('Do you like game of thrones?'); 
-likesGamesOfThrones = likesGamesOfThrones.toLowerCase(); 
-if (likesGamesOfThrones === "yes" ){
-  alert("Thats Awesome"); 
-}else if (likesGamesOfThrones === "somewhat"){
-  alert("I agree its Ok")
-}else{
-  alert ('That sucks'); 
-}
+var userName = prompt('Hello. What is your name?');
+console.log('The user name is ' + userName); 
+alert('Hi there ' + userName); 
 
-// var likesGamesOfThrones = prompt('Do you like game of thrones?'); 
-// likesGamesOfThrones = likesGamesOfThrones.toUpperCase(); 
-// if (likesGamesOfThrones === "YES" ){
-//   alert("Thats Awesome"); 
-// }else if (likesGamesOfThrones === "somewhat"){
-//   alert("I agree its Ok"); 
-// }else{
-//   alert ('That sucks'); 
-// }
+var age = prompt('What is your age?'); 
+console.log('Your age is ' + age); 
+alert(userName + " is "  + age + " old."); 
+
+var sport = prompt('What is your favourite sport?'); 
+console.log(userName + ' like ' + sport); 
+alert(userName + " is "  + age + " old and like " + sport); 
+
+var hobbies = prompt('How do you spend your free time?'); 
+console.log(userName + ' like ' + hobbies); 
+alert(userName + " is "  + age + " years old and like " + sport + "." + "He spends his time " + hobbies); 
